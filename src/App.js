c1d9e5b26f7e65f94d9d0c2a65c567542f888307
Rajdev Yadav <ryindia2023@gmail.com>
2023-04-19 15:21:52 -0400

    TextBox added.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import ImageOne from './components/ImageOne'
 import ImageThree from './components/ImageThree';
 import ImageTwo from './components/ImageTwo';
+import TextBox from './components/TextBox';
 function App() {
   return (
     <div className="App">
-    
-      <ImageOne/>
-      <ImageTwo/>
-      <ImageThree/>
+      <TextBox />
+      <ImageOne />
+      <TextBox />
+      <ImageTwo />
+      <TextBox />
+      <ImageThree />
+      <TextBox />
     </div>
   );
 }
---
+++ src/components/TextBox.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TextBox = () => {
+  return (
+    <div className='text-box'>
+      <h3>First Class Products</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni repudiandae voluptas, eius officia doloremque temporibus iste reiciendis cum dignissimos nihil nemo. Perspiciatis, ea sapiente numquam autem est saepe molestiae quae quasi repudiandae rem eaque repellat necessitatibus eos architecto dolor explicabo consequatur maiores cum ad, adipisci aspernatur corrupti? Debitis, pariatur eligendi! Magni dicta fugit, magnam impedit inventore ducimus est laborum repellat, ipsum repellendus quasi? Harum dicta dolorem a fugiat deserunt rem, ad nemo nobis, libero minima saepe voluptatum consequatur excepturi animi nisi. Iusto fuga ipsum dicta ratione, quasi enim! Exercitationem voluptate sint repudiandae eligendi, ex nesciunt facere dolore reiciendis modi deserunt.</p>
+    </div>
+  )
+}
+
+export default TextBox
